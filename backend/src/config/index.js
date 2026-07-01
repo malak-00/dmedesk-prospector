4788cc76daef6c442954d62a8d18eb6edc330134
malak-00 <158504859+malak-00@users.noreply.github.com>
2026-07-01 18:49:53 +0000

    Sprint 1.6 Add configuration layer

---
+++ backend/src/config/index.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config = {
+  port: process.env.PORT || 3000,
+  nodeEnv: process.env.NODE_ENV || "development",
+  nppesVersion: process.env.NPPES_VERSION || "2.1",
+};
+
+export default config;
