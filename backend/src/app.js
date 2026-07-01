d2b7e66d3956e25df23e0865ad9e1cae222fe5d6
malak-00 <158504859+malak-00@users.noreply.github.com>
2026-07-01 19:02:30 +0000

    Sprint 1.8 Add request logging middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
+import logger from "./middleware/logger.js";
 
 const app = express();
 
+app.use(logger);
 app.use(cors());
 app.use(express.json());
 
